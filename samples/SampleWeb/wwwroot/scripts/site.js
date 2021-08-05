ddc7a44ad0f1f39edb7013339dfc92d6485c57f5
Roderic Bos <github@rooc.nl>
2021-08-05 20:01:15 +0200

    bsToggle in samplesite

--- samples/SampleWeb/wwwroot/scripts/site.js
+++ samples/SampleWeb/wwwroot/scripts/site.js
@@ -1,1 +1,19 @@
-﻿
+﻿const toggles = document.querySelectorAll('[data-bs-toggle]');
+for (let i = 0; i < toggles.length; i++) {
+	const toggle = toggles[i];
+	toggle.addEventListener('click', () => {
+		const className = toggle.dataset.bsToggle;
+		const targets = document.querySelectorAll(toggle.dataset.bsTarget);
+		targets
+		for (let j = 0; j < targets.length; j++) {
+			const target = targets[j];
+			target
+			if (target.classList.contains(className)) {
+				target.classList.remove(className);
+			} else {
+				target.classList.add(className);
+			}
+		}
+	});
+}
+
